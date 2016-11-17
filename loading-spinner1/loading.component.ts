a5656ad913df80ba541fc5bd061090ad29d79976
Sunny Cheung <sunnycyk@yahoo.com>
2016-11-17 12:01:37 +0800

    loading spinner 1

---
+++ loading-spinner1/loading.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'loading-spinner',
+    templateUrl: 'loading.component.html',
+    styleUrls: ['loading.component.css'],
+
+})
+export class LoadingComponent {
+
+}
